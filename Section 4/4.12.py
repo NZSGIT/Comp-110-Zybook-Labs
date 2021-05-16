4fef7f0f734db81b0f7395b5cc0b3a4efc8679b1
NZSGIT <77271272+NZSGIT@users.noreply.github.com>
2021-05-15 23:24:24 -0700

    Create 4.12.py

---
+++ Section 4/4.12.py
@@ -0,0 +1,19 @@
+'''
+4.12 LAB: Smallest number
+Write a program whose inputs are three integers, and whose output is the smallest of the three values.
+
+Ex: If the input is:
+
+7
+15
+3
+the output is:
+
+3
+'''
+
+'''----------------------------------------------------------------------------------------------------------'''
+#Code Below
+
+list = [int(input()), int(input()), int(input())]
+print(min(list))
