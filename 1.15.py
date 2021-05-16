6bdb17c7fdebcfeb0fe45bb0d2c641653c33f97e
NZSGIT <77271272+NZSGIT@users.noreply.github.com>
2021-05-15 22:59:01 -0700

    Create 1.15.py

---
+++ 1.15.py
@@ -0,0 +1,10 @@
+'''
+1.15 LAB: Formatted output: No parking sign
+Write a program that prints a formatted "No parking" sign as shown below. Note the first line has two leading spaces. For ALL labs, end with newline (unless otherwise stated).
+'''
+
+'''----------------------------------------------------------------------------------------------------------'''
+#Code Below
+
+
+print("  NO PARKING\n2:00 - 6:00 a.m.")
