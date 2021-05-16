cc59e5aa182cba8bb2f654fe27f7529fe9eab3bc
NZSGIT <77271272+NZSGIT@users.noreply.github.com>
2021-05-15 22:59:57 -0700

    Create 1.16.py

---
+++ 1.16.py
@@ -0,0 +1,13 @@
+'''
+1.16 LAB: Input: Welcome message
+Write a program that takes a first name as the input, and outputs a welcome message to that name.
+
+Ex: If the input is Pat, the output is:
+'''
+
+'''----------------------------------------------------------------------------------------------------------'''
+#Code Below
+
+user_name = input()
+
+print("Hello {} and welcome to CS Online!".format(user_name))
