07569b617d592195a37f903cbbabd434f517261a
NZSGIT <77271272+NZSGIT@users.noreply.github.com>
2021-05-15 23:29:43 -0700

    Create 5.18.py

---
+++ Section 5/5.18.py
@@ -0,0 +1,32 @@
+'''
+5.18 LAB: Print string in reverse
+Write a program that takes in a line of text as input, and outputs that line of text in reverse. The program repeats, ending when the user enters "Done", "done", or "d" for the line of text.
+
+Ex: If the input is:
+
+Hello there
+Hey
+done
+then the output is:
+
+ereht olleH
+yeH
+'''
+
+'''----------------------------------------------------------------------------------------------------------'''
+#Code Below
+
+String = input()
+
+while (String.lower() != 'done' and String != 'd'):
+      
+    StringReverse = ''  
+    
+    for x in String:
+        StringReverse = x + StringReverse
+       
+    print(StringReverse)
+   
+    
+
+    String = input()
