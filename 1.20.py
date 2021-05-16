7d3524754e9c4ca2c0511f707c9552c45bd7af4e
NZSGIT <77271272+NZSGIT@users.noreply.github.com>
2021-05-15 23:04:00 -0700

    Create 1.20.py

---
+++ 1.20.py
@@ -0,0 +1,33 @@
+'''
+1.20 LAB*: Program: ASCII art
+This zyLab activity is the traditional programming assignment, typically requiring a few hours over a week. The previous sections provide warm up exercises intended to help a student prepare for this programming assignment.
+
+
+
+(1) Output this tree. (Submit for 2 points).
+
+   *
+  ***
+ *****
+*******
+  ***
+
+(2) Below the tree (with two blank lines), output this cat. (Submit for 3 points, so 5 points total).
+
+/\   /\
+  o o
+ =   =
+  ---
+Hint: A backslash \ in a string acts as an escape character, such as with a newline \n. So, to print an actual backslash, escape that backslash by prepending another backslash. Ex: The following prints a single backslash: print('\\')
+'''
+
+'''----------------------------------------------------------------------------------------------------------'''
+#Code Below
+
+print('   *')
+print('  ***')
+print(" *****\n*******\n  ***\n\n")
+
+print("/\\   /\\")
+print("  o o\n =   =\n  ---")
+
