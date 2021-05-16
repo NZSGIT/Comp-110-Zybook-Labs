83b17c4f33672ea67d8c6f731a158dd427a131de
NZSGIT <77271272+NZSGIT@users.noreply.github.com>
2021-05-15 22:57:57 -0700

    Create 1.14.py

---
+++ 1.14.py
@@ -0,0 +1,9 @@
+'''
+1.14 LAB: Formatted output: Hello World!
+Write a program that outputs "Hello World!" For ALL labs, end with newline (unless otherwise stated).
+'''
+
+'''----------------------------------------------------------------------------------------------------------'''
+#Code Below
+
+print("Hello World!")
